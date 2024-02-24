56a22e9dcc582df2daafd59bffbf65edf8865276
higa-shunnosuke <138739372+higa-shunnosuke@users.noreply.github.com>
2024-02-24 17:04:12 +0900

    環境構築

--- MyProject1/MyProject1/Common.h
+++ MyProject1/MyProject1/Common.h
@@ -1,1 +1,35 @@
 #pragma once
+#include"DxLib.h"
+
+/******************************************
+* マクロ定義
+******************************************/
+#define _USE_MATH_DEFINES	//数値演算定数
+#include <math.h>
+
+//画面の設定
+#define SCREEN_WIDTH (1280)
+#define SCREEN_HEIGHT (720)
+#define SCREEN_COLOBIT (32)
+#define FONT_SIZE (20)
+
+#define SCREEN_LEFT (0)
+#define SCREEN_RIGHT (1280)
+#define SCREEN_UPPER (0)
+#define SCREEN_UNDER (720)
+
+/****************************************************
+*入力制御機能：ゲーム終了用のボタン判定処理
+* 引　数：無し
+* 戻り値：TRUE（押した）,FALSE（押してない)
+*****************************************************/
+int GetExitButton(void)
+{
+	int ret = FALSE;
+
+	if ((CheckHitKey(KEY_INPUT_ESCAPE) == TRUE))
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
--- MyProject1/MyProject1/Main.cpp
+++ MyProject1/MyProject1/Main.cpp
@@ -1,21 +1,47 @@
 #include"DxLib.h"
+#include"Common.h"
+#include"FreamControl.h"
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,_In_
-	LPSTR lpCmdLine, _In_ int nShowCmd)
+/***********************************************
+ * プログラムの開始
+ ***********************************************/
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
 {
-	//ウィンドウオードで起動
-	ChangeWindowMode(TRUE);
+	double dNextTime = GetNowCount();
 
-	//Dxライブラリの初期化処理
-	if (DxLib_Init() == -1)
+	//タイトルを変更
+	SetMainWindowText("Profject1");
+
+	ChangeWindowMode(TRUE);		// ウィンドウモードで起動
+
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLOBIT);	//画面サイズの設定
+
+	if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
+
+	//フレーム制御機能初期化処理
+	FreamControl_Initialize();
+
+	SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
+
+
+	// ゲームループ
+	while ((ProcessMessage() == 0) && GetExitButton() != TRUE)
 	{
-		return -1;
-	}
+		//フレーム制御機能更新処理
+		FreamControl_Update();
 
-	//入力待機
-	WaitKey();
+		// 画面の初期化
+		ClearDrawScreen();
+
+
+		// 裏画面の内容を表画面に反映
+		ScreenFlip();
+	}
 
 	//Dxライブラリ使用の終了処理
 	DxLib_End();
+
+	//ソフトの終了
 	return 0;
 }
